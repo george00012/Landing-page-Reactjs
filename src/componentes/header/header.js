3fc809ec23497a259635104cbfd2b8e46fdacec7
george00012 <georgenarcis12@yahoo.es>
2023-05-17 19:13:57 +0200

    primera actualizacion

---
+++ src/componentes/header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../header/header.css";
+import arrow from "../header/img/icon-arrow-down.svg"
+
+const Header = () => {
+  return (
+    <div>
+      <h1>We are creatives</h1>
+      <div media="(max-width: 768px)" className="img-movil" />
+      <div media="(min-width: 768px)" className="img-pc" />
+      <img src={arrow} alt="arrow" className="arrow"/>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/componentes/nav/nav.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import logo from '../nav/img/logo.svg';
+import '../nav/Estilo.css';
+
+function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("open");
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <a className="navbar-brand" href="_blank">
+          <img src={logo} alt="Logo" className="logo" />
+        </a>
+        <button
+          className={`menu-toggle ${isMenuOpen ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <div className="hamburguer-menu">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+        </button>
+
+        <ul className="menu">
+          <li className="menu-item">
+            <a className="menu-link" href="_blank">
+              Home
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className="menu-link" href="_blank">
+              About
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className="menu-link" href="_blank">
+              Services
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className="menu-link contact" href="_blank">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
+
